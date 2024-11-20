475a35818cb1d5d753a7922aea565ac4a8eeda8b
jihadul4kbar <jihadul4kbar@gmail.com>
2024-11-20 17:29:19 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -140,7 +140,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Jihadul Akbar. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
